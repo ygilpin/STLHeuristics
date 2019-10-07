e22b6540bb1fb2f8ca1f876a287636daa81bea6e
Yann Gilpin <ygilpin@nd.edu>
2019-10-07 13:27:47 -0400

    Binary Tree appears to be working

--- STLpredicate.py
+++ STLpredicate.py
@@ -3,21 +3,23 @@
 import numpy as np
 
 class STLpredicate:
-    def __init__(self, t1, t2, ae, A, b, cdn = 0 , other = 0):
+    def __init__(self, t1 = 0, t2 = 0, ae = 0, A = 0, b = 0, cdn = 0 , left = 0, right = 0):
         self.t1 = t1 # When predicate is active
         self.t2 = t2
-        self.A  = A
-        self.b  = b
+        self.A  = A # Equation of a line
+        self.b  = b # constant from ^
         self.ae = ae # always or eventually
         self.cdn = cdn # conjuction, disjunction or negation
-        self.other = other # other STL predicate for conjuction/disjunction
+        self.left = left # other STL predicates for conjuction/disjunction
+        self.right = right
 
     def myRho(self, x, t):
-        #print("myRho called")
         # Calculate the robustness of myself
+        # Should only be called when I have no left or right branch
+        rhoVal = 'Nan'
         if self.t1 <= t and t <= self.t2:
             if self.ae == 'a':
-                self.rhoVal = self.b - self.A @ x[:,t]
+                rhoVal = self.b - self.A @ x[:,t]
             else:
                 # Check if there is satisfaction at any point
                 sat = False
@@ -28,54 +30,48 @@ class STLpredicate:
                 deltax = self.b - self.A @ x[:,t]
                 deltat = self.t2 -t
                 if sat:
-                    self.rhoVal = abs(deltat)/(abs(deltax)+1)
+                    rhoVal = abs(deltat)/(abs(deltax)+1)
                 else:
-                    self.rhoVal = -abs(deltax)/(abs(deltat)+1)
+                    rhoVal = -abs(deltax)/(abs(deltat)+1)
             if self.cdn == 'n':
-                self.rhoVal = -self.rhoVal
+                rhoVal = -rhoVal
 
-        else: 
-            self.rhoVal = "Nan"
-        return self.rhoVal
+        print("myRho called", rhoVal)
+        return rhoVal
 
     def Rho(self, x,t):
         #print("Robustness function called")
-        # Force the calcluation of my robustness
-        self.myRho(x, t)
-        # Now apply conjuction and disjunction
-        if self.other:
-            print("getting other robustness")
-            rho_o = self.other.Rho(x,t);
-            if self.cdn == 'c':
-                if self.rhoVal != "Nan" and rho_o != "Nan":
-                    return min(self.rhoVal, rho_o)
-                elif self.rhoVal == "Nan" and rho_o != "Nan":
-                    return rho_o
-                elif self.rhoVal != "Nan" and rho_o == "Nan":
-                    return self.rhoVal
-                else:
-                    return "NaN"
-            elif self.cdn == 'd':
-                if self.rhoVal != "Nan" and rho_o != "Nan":
-                    print("Maximizing robustness")
-                    return max(self.rhoVal, rho_o)
-                elif self.rhoVal == "Nan" and rho_o != "Nan":
-                    return rho_o
-                elif self.rhoVal != "Nan" and rho_o == "Nan":
-                    return self.rhoVal
-                else:
-                    return "NaN"
-        else:
-            return self.rhoVal
+        # Force the calcluation of left and right 
+        leftRho = 'Nan'
+        rightRho = 'Nan'
+        if self.left and self.right:
+            leftRho = self.left.Rho(x,t)
+            rightRho = self.right.Rho(x,t)
+            if leftRho != 'Nan' and rightRho != 'Nan':
+                if self.cdn == 'c':
+                    return min(leftRho, rightRho)
+                elif self.cdn == 'd':
+                    return max(leftRho, rightRho)
+            elif leftRho  == 'Nan' and rightRho != 'Nan':
+                return rightRho
+            elif leftRho != 'Nan' and rightRho == 'Nan':
+                return leftRho 
+        if self.left:
+            return left.Rho(x,t)
+        if self.right:
+            return right.Rho(x,t)
+        return self.myRho(x,t)
 
     def __invert__(self):
         return STLpredicate(self.t1, self.t2, self.ae, self.A, self.b, 'n', 0)
 
     def __add__(self, other):
-        return STLpredicate(other.t1, other.t2, other.ae, other.A, other.b, 'd', self)
+        print("Disjunction between two predicates")
+        return STLpredicate(cdn='d', left=self, right=other)
 
     def __mul__(self, other):
-        return STLpredicate(other.t1, other.t2, other.ae, other.A, other.b, 'c', self)
+        print("Conjunction between two predicates")
+        return STLpredicate(cdn='c', left=self, right=other)
 
     def arect(t1,t2,ae,x1,x2,y1,y2):
         # First the equations for the lines are needed
@@ -91,6 +87,7 @@ class STLpredicate:
 
 t1 = 0
 t2 = 4  
+time = 2
 A1 = np.array([1, 2])
 b1 = 5
 A2 = np.array([3, -5])
@@ -98,24 +95,7 @@ b2 = 3
 t = np.linspace(t1, t2, t2 + 1)
 x = np.vstack((t,2*t))
 
-x1 = 1
-x2 = 3
-y1 = 1
-y2 = 3
-ae = 'a'
-p1 = STLpredicate(t1, t2, ae, np.array([0, -1]), -y2)
-        # Bottom line
-p2 = STLpredicate(t1,t2,ae,np.array([0, -1]), y1)
-        # Left line
-p3 = STLpredicate(t1,t2,ae,np.array([1, 0]), x1)
-        # Right line
-p4 = STLpredicate(t1,t2,ae,np.array([-1, 0]), -x2)
-print("Testing the robustness of rectangle")
+p = STLpredicate.arect(t1, t2, 'a', 1, 3, 1, 3)
 for time in range(t1,t2+1):
     print(x[:,time])
-    print('p1', p1.Rho(x,time))
-    print('p2', p2.Rho(x,time))
-    print('p3', p3.Rho(x,time))
-    print('p4', p4.Rho(x,time))
     print(x[:,time], p.Rho(x,time))
-    print(x[:,time], p5.Rho(x,time))"""
